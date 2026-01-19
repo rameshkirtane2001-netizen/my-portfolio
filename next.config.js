4ad07cd815f3f7d715d9ed930f77f08f71e4eeed
rameshkirtane2001-netizen <rameshkirtane2001@gmail.com>
2026-01-19 13:48:01 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  experimental: {
+    typedRoutes: false,
   },
 }
 
