fc1d7ff2e247d67cbee2ee9ff4bd4f9e33c96660
rameshkirtane2001-netizen <rameshkirtane2001@gmail.com>
2026-01-19 13:43:21 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
-import { allBlogs } from 'contentlayer/generated'
-import Main from './Main'
-
-export default async function Page() {
-  const sortedPosts = sortPosts(allBlogs)
-  const posts = allCoreContent(sortedPosts)
-  return <Main posts={posts} />
+export default function HomePage() {
+  return (
+    <div>
+      <h1>My Portfolio</h1>
+      <p>UI/UX Designer</p>
+    </div>
+  )
 }
